a5399c056a30631491c533e0aff0fe5691b99ef9
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2016-12-16 10:43:33 +0700

    initial connit

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.d
@@ -0,0 +1,101 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Inc/stm32f4xx_hal_conf.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Inc/mxconstants.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Include/core_cm4.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Include/core_cmInstr.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Include/cmsis_gcc.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Include/core_cmFunc.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Include/core_cmSimd.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Inc/stm32f4xx_hal_conf.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Inc/mxconstants.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Include/core_cm4.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Include/core_cmInstr.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Include/core_cmFunc.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Include/core_cmSimd.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+F:/Academic/3rd\ \ Year/Semester1/Hw\ Lab/Final\ Project/Final/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
